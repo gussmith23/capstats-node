b1ac142e76d9e9123ac60f24dc79f3338bd1e0e8
Gus Smith <hfs5022@psu.edu>
2016-12-21 13:04:54 -0500

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/player', function(req, res, next){
+  var options = {
+    host: 'localhost',
+    port: 23232,
+    path: '/player'
+  };
+  
+  http.request(options, function(response){
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      res.render('player', { player : JSON.parse(str) });
+    });
+  }).end();
+  
 });
 
 module.exports = router;
