451af26c9d0c8444e21e8a57e19d6450aa618757
Gus Smith <gushenrysmith@gmail.com>
2016-12-22 21:40:13 -0500

    updates...

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/player', function(req, res, next){
-  var options = {
-    host: 'localhost',
-    port: 23232,
-    path: '/player'
-  };
-  
-  http.request(options, function(response){
-    var str = '';
-
-    //another chunk of data has been recieved, so append it to `str`
-    response.on('data', function (chunk) {
-      str += chunk;
-    });
-
-    //the whole response has been recieved, so we just print it out here
-    response.on('end', function () {
-      res.render('player', { player : JSON.parse(str) });
-    });
-  }).end();
-  
-});
-
 module.exports = router;
---
+++ routes/player.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var request = require('request');
+
+router.get('/:id?', function(req, res, next){
+  var path = "/player";
+  var showForm = true;
+
+  // Route parameter (id) means the user is looking
+  // for a specific player.
+  if (req.params.id) {
+    path = "/player/" + req.params.id;
+    showForm = false;
+  }
+
+  // Path parameters means the user's doing a search.
+  else if (Object.keys(req.query).length) {
+    path = "/player" +  req.url.slice(1);
+    showForm = true;
+  }
+
+  var options = {
+    host: 'localhost',
+    port: 23232,
+    path: path
+  };
+
+  http.request(options, function(response){
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+        str += chunk;
+        });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+        res.render('player', { player : JSON.parse(str), showForm : showForm });
+        });
+  }).end();
+
+});
+
+router.post('/', function(req, res, next) {
+  var playerJson = {};
+
+  const name = req.body.name;
+  playerJson.name = name.length > 0 ? name : null;
+  const telegramId = parseInt(req.body.telegramId);
+  playerJson.telegramId = telegramId >= 0 ? telegramId : null;
+  const telegramUsername = req.body.telegramUsername;
+  playerJson.telegramUsername = telegramUsername.length > 0 ? telegramUsername : null;
+
+  console.dir(playerJson);
+
+  request.post(
+    'http://localhost:23232/player',
+    {json: playerJson},
+    function (error, response, body) {
+      if (error || response.statusCode != 200) return;
+      //console.log(body);
+    });
+
+  res.redirect('/player');
+
+});
+
+module.exports = router;
